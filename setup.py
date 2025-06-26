92a84df8a0c9c8e75edff557375e15dc1afed479
Hillol Pratim Kalita <101858515+ringerH@users.noreply.github.com>
2025-06-26 12:09:31 +0000

    setup run

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup, find_packages
+from typing import List
+HYPHEN_E_DOT = '-e .'
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
